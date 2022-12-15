74459a88908536f50785ec5a96f4bd17ade00307
Crowdbotics <team@crowdbotics.com>
2022-12-15 17:17:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lagniappe_legends_37910",
+  url: "https://lagniappe_legends_37910.botics.co",
+  api: "https://lagniappe_legends_37910.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
